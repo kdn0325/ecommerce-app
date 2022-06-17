df2f6a8dbd6245733ca02fc9f056975ccdad8658
kdn0325 <sexy-ehdsud@nate.com>
2022-06-17 18:12:50 +0900

    REST-API Stripe.js Cart.js add updating

--- components/Cart.js
+++ components/Cart.js
@@ -1,9 +1,103 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus , AiOutlinePlus , AiOutlineLeft , AioutlineShopping, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline} from "react-icons/ti"
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
+import Numeral from "numeral"
+import toast from 'react-hot-toast';
 
 const Cart = () => {
+    const cartRef = useRef();
+    const {totalPrice , totalQuantities , cartItems , setShowCart ,toggleCartItemQuanitity , onRemove} = useStateContext();
+
+    const handleCheckOut = async () =>{
+        const stripe = await getStripe();
+
+        const responce = await fetch('/api/stripe' , {
+            method:'POST',
+        
+            headers: {
+                'Content-Type' : 'application/json' , 
+            },
+
+            body:JSON.stringify(cartItems),
+
+        });
+
+        if(responce.statusCode === 500) return;
+
+        const data = await responce.json();
+
+        toast.loading("로딩중 ...");
+
+        stripe.redirectToCheckout({sessionId : data.id})
+    }
     return (
-        <div>
-            Cart
+        <div className="cart-wrapper" ref={cartRef}>
+            <div className="cart-container">
+                <button type="button" className="cart-heading" onClick={()=>setShowCart(false)}>
+                    <AiOutlineLeft/>
+                    <span className="heading">장바구니</span>
+                    <span className="cart-num-items">({totalQuantities} items)</span>
+                </button>
+                {cartItems.length < 1 && (
+                    <div className="empty-cart">
+                        <AiOutlineShopping size={150}/>
+                        <h3>장바구니가 비어있습니다</h3>
+                        <Link href="/">
+                            <button type="button" className="btn" onClick={()=>setShowCart(false)}>
+                               쇼핑 하기 
+                            </button>
+                        </Link>
+                    </div>
+                )}
+                <div className="product-container">
+                    {cartItems.length >= 1 && cartItems.map((item) => (
+                        <div className="product" key={item._id}>
+                            <img src={urlFor(item?.image[0])} className="cart-product-image"/>
+                            <div className="item-desc">
+                                <div className="flex top">
+                                    <h5>{item.name}</h5>
+                                    <h4>{Numeral(item.price).format(0,0)}원</h4>
+                                </div>
+                                <div className="flex bottom">
+                                    <div>
+                                    <p className="quantity-desc">
+                                        <span className="minus" onClick={()=>toggleCartItemQuanitity(item._id,"dec")}>
+                                            <AiOutlineMinus/>
+                                        </span>
+                                        <span className="num" onClick="">
+                                            {item.quantity}
+                                        </span>
+                                        <span className="plus" onClick={()=>toggleCartItemQuanitity(item._id,"inc")}>
+                                            <AiOutlinePlus/>
+                                        </span>
+                                    </p>
+                                    </div>
+                                    <button type="button" className="remove-item" onClick={()=>onRemove(item)}>
+                                        <TiDeleteOutline/>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {cartItems.length >=1 && (
+                    <div className="cart-bottom">
+                        <div className="total">
+                            <h3>총 가격:</h3>
+                            <h3>{Numeral(totalPrice).format(0,0)}원</h3>
+                        </div>
+                        <div className="btn-container">
+                            <button type="button" className="btn" onClick={handleCheckOut}>
+                                구매하기
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
--- context/StateContext.js
+++ context/StateContext.js
@@ -1,5 +1,5 @@
 import React , {createContext , useContext , useState, useEffect} from "react";
-import toast, { Toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 //Context 객체 생성
 
@@ -8,9 +8,12 @@ const Context = createContext();
 export const StateContext = ({children}) => {
     const [showCart , setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice , setTotalPrice] = useState();
-    const [totalQuantities , setTotalQuantities] = useState()
+    const [totalPrice , setTotalPrice] = useState(0);
+    const [totalQuantities , setTotalQuantities] = useState(0)
     const [qty , setQty ] = useState(1);
+    
+    let foundProduct;
+    let index;
 
     //장바구니 추가 
     const onAdd = (product, quantity) => {
@@ -25,7 +28,7 @@ export const StateContext = ({children}) => {
        
        if(checkProductInCart){
            const updateCartitems = cartItems.map((cartProduct) => {
-               //장바구니 상품과 상품이 같으면
+               //장바구니 상품과 상품이 같은 경우
                if(cartProduct._id === product._id) return {
                    ...cartProduct,
                    //수량은 장바구니 수량
@@ -38,12 +41,67 @@ export const StateContext = ({children}) => {
             
             setCartItems([...cartItems,{...product}]);
         }
-        toast.success(`${qty} ${product.name} 장바구니에 추가 되었습니다.`)
-       };
+        toast.success(`${qty}개의 ${product.name}가 장바구니에 추가 되었습니다.`)
+        };
+    
+    //장바구니 제거
+    const onRemove = (product) =>{
+        foundProduct = cartItems.find(item=>item._id === product._id);
 
+        //선택한 상품 품목만 제거
+        const newCartItems = cartItems.filter(item=>item._id !== product._id);
+
+        //총 가격  = 제품 가격 * 수량만큼 제거
+        setTotalPrice(prevTotalPrice=>prevTotalPrice - foundProduct.price * foundProduct.quantity);
+        //총 갯수  = 제품 수량 * 수량만큼 제거
+        setTotalQuantities(prevTotalQuantities=>prevTotalQuantities - foundProduct.quantity);
+
+        //
+        setCartItems(newCartItems);
+    }
+
+    //장바구니 아이템 메뉴 증가 / 감소
+    const toggleCartItemQuanitity = (id, value) =>{
+        //
+        foundProduct = cartItems.find(item=>item._id === id)
+
+        //선택한 id값과 같은 index 번호 검색
+        index = cartItems.findIndex(product=>product._id===id)
+        //
+        const newCartItems = cartItems.filter((item,)=>item._id !== id )
+        
+        //inc 값이면 장바구니 아이템 메뉴 증가
+        if(value==="inc"){
+            //상품 추가 목록에서 선택한 상품 추가
+            setCartItems([...newCartItems , {...foundProduct , quantity:foundProduct.quantity + 1}])
+
+            //전체 상품에서 선택한 상품만큼 가격 추가
+            setTotalPrice((prevTotalPrice)=>prevTotalPrice+foundProduct.price)
+
+            //전체 수량에서 선택한 상품만큼 수량 추가
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+
+        //dec 값이면 장바구니 아이템 메뉴 증가
+        }else if(value=="dec"){
+            //제품이 1개 이상의 수량일 경우에만 조건
+            if(foundProduct.quantity > 1 ){
+
+                //상품 추가 목록에서 선택한 상품 제거
+                setCartItems([...newCartItems , {...foundProduct , quantity:foundProduct.quantity - 1}])
+
+                //전체 상품에서 선택한 상품만큼 가격 감소
+                setTotalPrice((prevTotalPrice)=>prevTotalPrice-foundProduct.price)
+
+                //전체 상품에서 선택한 수량만큼 가격 감소
+                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
+        }
+    }
+    //증가 
     const incQty = () =>{
         setQty((prevQty)=> prevQty+1)
     }
+    //감소
     const decQty = () =>{
         setQty((prevQty)=> {
             
@@ -56,7 +114,7 @@ export const StateContext = ({children}) => {
     return (
 
         //value prop을 받아서 하위 컴포넌트에게 전달함
-        <Context.Provider value={{showCart,cartItems,totalPrice,totalQuantities,qty,incQty,decQty,onAdd}}>
+        <Context.Provider value={{showCart,cartItems,totalPrice,totalQuantities,qty,incQty,decQty,onAdd,setShowCart,toggleCartItemQuanitity,onRemove}}>
             {children}
         </Context.Provider>
     )
