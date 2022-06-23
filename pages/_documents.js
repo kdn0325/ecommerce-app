f072044a6be338b8387bd65b1fcc52798f03ab5f
kdn0325 <sexy-ehdsud@nate.com>
2022-06-23 20:02:40 +0900

    update

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,6 +1,4 @@
-import Document, {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -32,7 +30,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <style />
+          <style/>
         </Head>
         <body>
           <Main />
