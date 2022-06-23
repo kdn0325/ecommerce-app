e9260623b8a9dd317354fa508b1345ca5e78f0b8
kdn0325 <sexy-ehdsud@nate.com>
2022-06-23 20:12:14 +0900

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Layout} from "../components"
 import { StateContext } from '../context/StateContext';
 import { Toaster } from 'react-hot-toast';
-import GlobalStyle from '../styles/Globalstyle';
+import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
   return(
     <StateContext>
+        <GlobalStyle/>
         <Layout>
-          <GlobalStyle/>
           <Toaster/>
           <Component {...pageProps} />
         </Layout>
